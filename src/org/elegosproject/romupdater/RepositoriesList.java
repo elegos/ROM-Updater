dbe388aa1f378905abdb0cc98397b45796888628
Giacomo Furlan <elegos@fastwebnet.it>
2011-01-29 00:34:28 +0100

    Fixed that crap code generated by git

--- src/org/elegosproject/romupdater/RepositoriesList.java
+++ src/org/elegosproject/romupdater/RepositoriesList.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
