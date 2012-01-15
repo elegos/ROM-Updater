5a232a766aaac0bb930ddb12674702333d3ed6fc
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-01-16 00:17:03 +0100

    Initial commit to build in cm7 repo

---
+++ Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_NAME := libGson
+LOCAL_MODULE_TAGS := optional
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libGson:Gson/gson-1.6.jar
+include $(BUILD_MULTI_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_PACKAGE_NAME := RomUpdater
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := shared
+LOCAL_SRC_FILES := \
+	$(call all-java-files-under, google) \
+	$(call all-java-files-under, src) \
+
+LOCAL_STATIC_JAVA_LIBRARIES := libGson
+
+# LOCAL_JAVACFLAGS += -Xlint:unchecked
+
+include $(BUILD_PACKAGE)
+
