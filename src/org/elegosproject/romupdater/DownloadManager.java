f81e6ffd066689b544f228ffdc9981e71b3fd11b
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-01-24 02:51:29 +0100

    Rewrite http downloader to support redirections and cookies

--- src/org/elegosproject/romupdater/DownloadManager.java
+++ src/org/elegosproject/romupdater/DownloadManager.java
@@ -17,88 +17,240 @@
 
 package org.elegosproject.romupdater;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.List;
+import java.util.Map;
+
+import java.security.cert.*;
+import javax.net.ssl.*;
 
 import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
+//import org.apache.http.params.HttpParams;
 
+import android.text.TextUtils;
 import android.util.Log;
+import android.content.Context;
 
 public class DownloadManager {
-	private static final String TAG = "ROM Updater (DownloadPackage.class)";
-	public static final String download_path = "romupdater/";
-	
-	public static boolean checkHttpFile(String url) {
-		try {
-			HttpParams httpParameters = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout(httpParameters,3000);
-			Log.i(TAG,"Testing "+url+"...");
-			URL theUrl = new URL(url);
-			HttpURLConnection connection = (HttpURLConnection) theUrl.openConnection();
-			if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				connection.disconnect();
-			} else { Log.i(TAG,"HTTP Response code: "+connection.getResponseCode()); return false; }
-		} catch (IOException e) {
-			Log.e(TAG,e.toString());
-			return false;
-		}
-		return true;
-	}
-	
-	public static boolean sendAnonymousData() {
-		String link = "http://www.elegosproject.org/android/upload.php";
-		String data;
-		
-		SharedData shared = SharedData.getInstance();
-		String romName = shared.getRepositoryROMName();
-		String romVersion = shared.getDownloadVersion();
-		String romPhone = shared.getRepositoryModel();
-		String romRepository = shared.getRepositoryUrl();
-		
-		if(romName.equals("") ||
-				romVersion.equals("") ||
-				romPhone.equals("") ||
-				romRepository.equals("")) {
-			Log.e(TAG,"Internal error - missing system variables.");
-			return false;
-		}
-		
-		if(!checkHttpFile(link)) return false;
-		try {
-			data = URLEncoder.encode("phone", "UTF-8") + "=" + URLEncoder.encode(romPhone, "UTF-8");
-			data += "&"+URLEncoder.encode("rom_name", "UTF-8") + "=" + URLEncoder.encode(romName, "UTF-8");
-			data += "&"+URLEncoder.encode("rom_version", "UTF-8") + "=" + URLEncoder.encode(romVersion,"UTF-8");
-			data += "&"+URLEncoder.encode("rom_repository", "UTF-8") + "=" + URLEncoder.encode(romRepository,"UTF-8");
+    private static final String TAG = "ROM Updater (DownloadPackage.class)";
+    public static final String download_path = "romupdater/";
 
-			HttpParams httpParameters = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout(httpParameters,3000);
-			
-			URL url = new URL(link);
-			url.openConnection();
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("User-Agent", "ROMUpdater");
-			conn.setDoOutput(true);
-			PrintWriter out = new PrintWriter(conn.getOutputStream());
-			out.println(data);
-			out.close();
-			
-			int status = Integer.parseInt(conn.getHeaderField("ROMUpdater-status"));
-			if(status == 1)
-				return true;
+    // always verify the host - dont check for certificate
+    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    };
 
-			Log.e(TAG, "It was impossible to send data to the stastistics server ("+conn.getHeaderField("ROMUpdater-error")+").");
-			return false;
-				
-		} catch (Exception e) {
-			Log.e(TAG, "It was impossible to send data to the stastistics server.");
-			Log.e(TAG, "Error: "+e.toString());
-			return false;
-		}
-	}
+    /**
+     * Trust every server - dont check for any certificate
+     */
+    private static void trustAllHosts() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[] {};
+            }
+            public void checkClientTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {}
+            public void checkServerTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {}
+        } };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String referer;
+    public static String cookies;
+
+    public static boolean checkHttpFile(String theUrl) {
+        try {
+            Log.i(TAG, "Testing "+theUrl+"...");
+            URL url = new URL(theUrl);
+            HttpURLConnection conn;
+            if (url.getProtocol().toLowerCase().equals("https")) {
+                trustAllHosts();
+                HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+                https.setHostnameVerifier(DO_NOT_VERIFY);
+                Log.d(TAG,"...using https protocol");
+                conn = https;
+            } else {
+                conn = (HttpURLConnection) url.openConnection();
+            }
+            conn.setInstanceFollowRedirects(false);
+            conn.setRequestMethod("HEAD");
+            conn.setConnectTimeout(3000);
+            if (!TextUtils.isEmpty(cookies)) {
+                conn.addRequestProperty("Referer", referer);
+                conn.addRequestProperty("Cookie", cookies);
+                Log.d(TAG, "Cookie sent :" + cookies);
+            }
+            conn.connect();
+            String setcookie="";
+            switch (conn.getResponseCode()) {
+            case HttpURLConnection.HTTP_OK:
+                Log.d(TAG, "HTTP OK");
+                conn.getInputStream().close();
+                conn.disconnect();
+                break;
+            case HttpURLConnection.HTTP_MOVED_TEMP:
+            case HttpURLConnection.HTTP_MOVED_PERM:
+                referer=theUrl;
+                String loc = conn.getHeaderField("Location");
+                conn.getInputStream().close();
+                Log.d(TAG, "HTTP redirect to " + loc);
+                try {
+                    setcookie = conn.getHeaderField("Set-Cookie");
+                } catch (Exception e) {}
+                if (TextUtils.isEmpty(cookies) && !TextUtils.isEmpty(setcookie)) {
+                    if (setcookie.contains("expires")) {
+                        setcookie = setcookie.substring(0, setcookie.indexOf("expires"));
+                        setcookie = setcookie.substring(0, setcookie.lastIndexOf(";"));
+                    }
+                    cookies = setcookie;
+                    Log.i(TAG, "Cookies received " + cookies);
+                }
+                conn.getInputStream().close();
+                return checkHttpFile(loc);
+                //throw new RedirectRequiredException(httpConn);
+            default:
+                referer=theUrl;
+                try {
+                    setcookie = conn.getHeaderField("Set-Cookie");
+                } catch (Exception e) {}
+                if (TextUtils.isEmpty(cookies) && !TextUtils.isEmpty(setcookie)) {
+                    if (setcookie.contains("expires")) {
+                        setcookie = setcookie.substring(0, setcookie.indexOf("expires"));
+                        setcookie = setcookie.substring(0, setcookie.lastIndexOf(";"));
+                    }
+                    cookies = setcookie;
+                    Log.i(TAG, "Cookies received " + cookies);
+                    conn.getInputStream().close();
+                    return checkHttpFile(theUrl);
+                }
+                Log.w(TAG, "HTTP Response code: "+conn.getResponseCode());
+                Map<String, List<String>> hf = conn.getHeaderFields();
+                for (String key : hf.keySet())
+                    Log.d(TAG, key + ": " + conn.getHeaderField(key));
+
+                int size = conn.getContentLength();
+                int index = 0;
+                int current = 0;
+                File file = new File("/sdcard/404_debug.txt");
+                try {
+                    FileOutputStream output = new FileOutputStream(file);
+                    InputStream input = conn.getInputStream();
+                    BufferedInputStream buffer = new BufferedInputStream(input);
+                    byte[] bBuffer = new byte[4096];
+
+                    while((current = buffer.read(bBuffer)) != -1) {
+                        try {
+                            output.write(bBuffer, 0, current);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        index += current;
+                    }
+                    output.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return false;
+                }
+
+                conn.disconnect();
+                Log.e(TAG, "check content of /sdcard/404_debug.txt");
+                return false;
+            }
+        } catch (IOException e) {
+            Log.e(TAG,e.toString());
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean sendAnonymousData(Context ctx) {
+
+        // <string name="statserver_url">http://www.elegosproject.org/android/upload.php</string>
+
+        String link = ctx.getResources().getString(R.string.statserver_url);
+        String data;
+
+        Log.d(TAG, "Stat report url set to " + link);
+
+        SharedData shared = SharedData.getInstance();
+        String romName = shared.getRepositoryROMName();
+        String romVersion = shared.getDownloadVersion();
+        String romPhone = shared.getRepositoryModel();
+        String romRepository = shared.getRepositoryUrl();
+
+        if(romName.equals("") ||
+                romVersion.equals("") ||
+                romPhone.equals("") ||
+                romRepository.equals("")) {
+            Log.e(TAG,"Internal error - missing system variables.");
+            return false;
+        }
+
+        cookies = "";
+        if(!checkHttpFile(link)) return false;
+        try {
+            data = URLEncoder.encode("phone", "UTF-8") + "=" + URLEncoder.encode(romPhone, "UTF-8");
+            data += "&"+URLEncoder.encode("rom_name", "UTF-8") + "=" + URLEncoder.encode(romName, "UTF-8");
+            data += "&"+URLEncoder.encode("rom_version", "UTF-8") + "=" + URLEncoder.encode(romVersion,"UTF-8");
+            data += "&"+URLEncoder.encode("rom_repository", "UTF-8") + "=" + URLEncoder.encode(romRepository,"UTF-8");
+
+            URL url = new URL(link);
+            HttpURLConnection conn;
+            if (url.getProtocol().toLowerCase().equals("https")) {
+                trustAllHosts();
+                HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+                https.setHostnameVerifier(DO_NOT_VERIFY);
+                conn = https;
+            } else {
+                conn = (HttpURLConnection) url.openConnection();
+            }
+            conn.setFollowRedirects(true);
+            conn.setConnectTimeout(3000);
+            conn.setReadTimeout(3000);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("User-Agent", "ROMUpdater");
+            if (!TextUtils.isEmpty(cookies)) {
+                conn.addRequestProperty("Cookie", cookies);
+            }
+            conn.setDoOutput(true);
+            PrintWriter out = new PrintWriter(conn.getOutputStream());
+            out.println(data);
+            out.close();
+
+            int status = Integer.parseInt(conn.getHeaderField("ROMUpdater-status"));
+            if(status == 1)
+                return true;
+
+            Log.e(TAG, "It was impossible to send data to the stastistics server ("+conn.getHeaderField("ROMUpdater-error")+").");
+            return false;
+
+        } catch (Exception e) {
+            Log.e(TAG, "It was impossible to send data to the stastistics server.");
+            Log.e(TAG, "Error: "+e.toString());
+            return false;
+        }
+    }
 }
