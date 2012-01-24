84a1d02c72773f44f5dad11f2e9793afddfcd2ea
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-01-24 06:56:59 +0100

    cleanspec: add libGson too

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -8,6 +8,7 @@
 # $(call add-clean-step, find $(OUT_DIR) -type f -name "IGTalkSession*" -print0 | xargs -0 rm -f)
 # $(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
 
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/libGson_intermediates)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/RomUpdater_intermediates)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/RomUpdater_intermediates)
 
